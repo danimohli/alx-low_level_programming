0977815a800ae8e43ac5166a2666847150df88ce
danimohli <danimohlife@gmail.com>
2023-12-14 06:16:50 -0800

    output with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - starting point of programe
+ * Return: 0 On Successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
