cb11fb972dee0617ba56308c602d5371dc35c1a8
danimohli <danimohlife@gmail.com>
2023-12-15 14:47:22 -0800

    output num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - starting point for other function
+ * Return: 0 on successful
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+		printf("%d", 1++);
+	return (0);
+}
