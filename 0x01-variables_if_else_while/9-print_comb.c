5b243a66de667580f10d5fcbbc5f0a796d3db80f
danimohli <danimohlife@gmail.com>
2023-12-15 15:41:03 -0800

    combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,14 @@ int main(void)
 
 	while (i <= 9)
 	{
-		putchar('0' + i++);
+		putchar('0' + i);
 
 		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+		i++;
 	}
 	putchar('\n');
 	return (0);
