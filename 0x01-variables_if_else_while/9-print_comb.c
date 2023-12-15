434056c04afd82954a55727f6dfdf4e0aeba8a87
danimohli <danimohlife@gmail.com>
2023-12-15 15:37:38 -0800

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar('0' + i++);
+
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
