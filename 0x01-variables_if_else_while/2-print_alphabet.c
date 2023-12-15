eecc7fe7f59395c67ac695f301dced16945387cd
danimohli <danimohlife@gmail.com>
2023-12-15 14:28:59 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * Return: 0 on Successful
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
