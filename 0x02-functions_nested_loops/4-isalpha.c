b431f2c642bb9d2675d5fe97cf247fd6eddd6cc2
danimohli <danimohlife@gmail.com>
2023-12-19 04:17:51 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  */
 int _isalpha(int c)
 {
-	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A')
-			return (1);
+	if ((c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A'))
+		return (1);
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * times_table - times table of 9
+ * Return: void
+ */
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+
+			if (c > 9)
+				_putchar()
+		}
+	}
+}
