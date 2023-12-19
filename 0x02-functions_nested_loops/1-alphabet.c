d38bd5b11bc26e0c6875547415252b3e82bb5c79
danimohli <danimohlife@gmail.com>
2023-12-19 02:03:52 -0800

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+/**
+ * print_alphabet - A function
+ * Function  that Print Alphabet
+ *
+ * Return: 0 on succssful
+ */
+void print_alphabet(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+		_putchar(chr);
+	_putchar('\n');
+}
