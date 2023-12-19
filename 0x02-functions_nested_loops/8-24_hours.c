5826381e05b2e409ba65893f2089f70586526ac9
danimohli <danimohlife@gmail.com>
2023-12-19 03:20:24 -0800

    24 hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - jack bauer (24 hrs)
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hr, sec;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (sec = 0; sec < 60; sec++)
+		{
+			_putchar(hr / '0');
+			_putchar(hr % '0');
+			_putchar(':');
+			_putchar(sec / '0');
+			_putchar(sec % '0');
+			_putchar('\n');
+		}
+	}
+}
