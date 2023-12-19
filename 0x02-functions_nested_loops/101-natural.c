ef97e0f5f6b4093bc180284cfb4cc9015698ca8b
danimohli <danimohlife@gmail.com>
2023-12-19 14:03:13 -0800

    sum of natural num

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * printing sum of multiple of 3 & 5 below 1024
+ * Return: 0 on successful
+ */
+int main(void)
+{
+	int x = 3, sum = 0;
+
+	while (x < 1024)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+			sum += x;
+		x++;
+	}
+	return (0);
+}
