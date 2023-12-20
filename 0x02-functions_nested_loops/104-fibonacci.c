2b7f287903791377009980d30af483770a4e7b1c
danimohli <danimohlife@gmail.com>
2023-12-20 12:44:50 -0800

    fib of 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,13 +8,14 @@ int main(void)
 	unsigned int fib1 = 1;
 	unsigned int fib2 = 2;
 	int i;
+
 	printf("%u, %u", fib1, fib2);
-	
-	for (i = 2; i < 98; i++)
+	for (i = 2; i <= 98; i++)
 	{
 		unsigned int next = fib1 + fib2;
+
 		printf(", %u", next);
-		
+
 		fib1 = fib2;
 		fib2 = next;
 	}
