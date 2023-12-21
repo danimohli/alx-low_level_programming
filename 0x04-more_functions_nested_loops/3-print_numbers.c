e841d46450a988ea77ee18984f6fde959d22c551
danimohli <danimohlife@gmail.com>
2023-12-21 06:51:53 -0800

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print 0 - 9
+ * Return: void function
+ */
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+		_putchar(c + '0');
+	_putchar('\n');
+}
