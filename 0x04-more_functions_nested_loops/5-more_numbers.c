634ff3be7b3ee98eda8229e1859ca0cf05627d4d
danimohli <danimohlife@gmail.com>
2023-12-21 07:27:57 -0800

    0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - print 0 - 14
+ * Return: void function
+ */
+void more_numbers(void)
+{
+	int x = 0, y;
+
+	while (x <= 9)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y < 10)
+				_putchar(y + '0');
+			else
+			{
+				_putchar(y / 10 + '0');
+				_putchar(y % 10 + '0');
+			}
+		}
+		_putchar('\n');
+		x++;
+	}
+}
