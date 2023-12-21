27ca44cf7963e2b82b6cffbd05629f7f82afca4c
danimohli <danimohlife@gmail.com>
2023-12-21 13:02:16 -0800

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 
 	for (n = 3; n <= 100; n++)
 	{
-		if (n % 1 == 0 && n % 5 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
 			printf("FizzBuzz ");
 		else if (n % 3 == 0)
 			printf("Fizz ");
