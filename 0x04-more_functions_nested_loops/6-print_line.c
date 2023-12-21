5356203c558aaeee7cc8bbbf6c51c45cc49d6ec8
danimohli <danimohlife@gmail.com>
2023-12-21 07:53:08 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line  - print line
+ * Return: void function
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		for (m = 0; m < n; m++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
