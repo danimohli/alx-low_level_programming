a50856125643888ed55c218c51d41dfb2252e2ea
danimohli <danimohlife@gmail.com>
2024-01-02 04:21:02 -0800

    change value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/**
+ * reset_to_98 - function that take pointer as argument to int and then change
+ * the value to 98 from within the function
+ * Return: void function
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+On the jouney to learning C programming, this project touch on pointer; how  to declare it, use it.
