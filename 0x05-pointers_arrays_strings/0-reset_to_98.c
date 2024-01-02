d6f2ee9847e3f732ecabf35d9e1a66d11e8c7e51
danimohli <danimohlife@gmail.com>
2024-01-02 04:23:06 -0800

    change value to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,7 @@
 /**
  * reset_to_98 - function that take pointer as argument to int and then change
  * the value to 98 from within the function
+ * @n: pointer to value derefference
  * Return: void function
  */
 void reset_to_98(int *n)
