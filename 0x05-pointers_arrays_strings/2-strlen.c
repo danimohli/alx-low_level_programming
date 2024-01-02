02336e344e0de3d4c58a72a620d1ff06eafbadaa
danimohli <danimohlife@gmail.com>
2024-01-02 05:03:36 -0800

    len of str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ * _strlen - count numbr of char in str
+ * @s: pointer to str
+ * Return: len of str
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return len;
+}
