8c9a284d5b0ca2888a0049c57db8bf80119a77cd
danimohli <danimohlife@gmail.com>
2024-01-04 11:30:13 -0800

    top still workin on

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include <string.h>
+/**
+ * string_toupper - change all letters to upper case
+ * @str: string to convert
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int length = strlen(str);
+
+	for (int i = 0; i < length; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+	}
+	return (str);
+}
