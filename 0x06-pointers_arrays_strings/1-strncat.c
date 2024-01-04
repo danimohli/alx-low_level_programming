b76dfaf54d032b1c0ef8cf43e485c7c1f851aca9
danimohli <danimohlife@gmail.com>
2024-01-04 03:17:24 -0800

    concat src[x] ==> *src

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,16 +3,16 @@
  * _strncat - similar to strcat but on use nth
  * @dest: where to copy to
  * @src: copy from
+ * @n: the nth
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int len = strlen(dest), x = 0;
 
-	while (*src != '\0' && x < n)
+	for (x = 0; x < n && *src != '\0'; x++)
 	{
-		dest[len] = src[x];
-		x++;
+		dest[len] = *src;
 		src++;
 		len++;
 	}
