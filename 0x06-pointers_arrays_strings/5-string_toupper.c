bd5d7a928c04df8e297d21254eb29a90e9bf5a22
danimhli <danimhlife@gmail.com>
2024-01-04 20:41:41 +0100

    to upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,9 +6,9 @@
  */
 char *string_toupper(char *str)
 {
-	int length = strlen(str);
+	int length = strlen(str), i;
 
-	for (int i = 0; i < length; i++)
+	for (i = 0; i < length; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
