457b88d4df7ed18f7a70faa1452eac3c24a67282
danimohli <danimohlife@gmail.com>
2024-01-08 12:16:29 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+/**
+ * _strstr - find the first occurence of the substring
+ * @haystack: strng
+ * @needle: the substring
+ * Return: located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return ('\0');
+}
