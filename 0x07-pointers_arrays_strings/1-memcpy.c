6dd7497caa1c0da227eb0c2263aaa655603f166f
danimohli <danimohlife@gmail.com>
2024-01-08 03:18:17 -0800

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _memcpy - copy memory
+ * @dest: destination to copy to
+ * @src: srcour where to copy from
+ * @n: nth
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x = 0;
+
+	while (x < n)
+	{
+		*dest = src[x];
+		dest++;
+		x++;
+	}
+	return (dest);
+}
