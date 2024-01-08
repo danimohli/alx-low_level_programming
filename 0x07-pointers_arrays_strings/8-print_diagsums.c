762fee1b71ec19e9e472d6bb17218a7aa42085b3
danimohli <danimohlife@gmail.com>
2024-01-08 15:08:48 -0800

    add diaonaly

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * print_diagsums - take nums diagonally and sum
  * @a: array of int
@@ -6,5 +7,13 @@
  */
 void print_diagsums(int *a, int size)
 {
+	int diag1_sum = 0, diag2_sum = 0;
 
+	for (int i = 0; i < size; i++)
+	{
+		diag1_sum += a[i * size + i];
+		diag2_sum += a[i * size + (size - 1 - i)];
+	}
+	printf("%d\n", diag1_sum);
+	printf("%d\n", diag2_sum);
 }
