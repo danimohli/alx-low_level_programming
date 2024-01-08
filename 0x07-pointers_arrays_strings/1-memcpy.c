b74fcb7a2494fd106c5ac6d73f09e24dd4e6420e
danimohli <danimohlife@gmail.com>
2024-01-08 03:57:42 -0800

    memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+/**
+ * _memcpy - copy memory
+ * @dest: destination to copy to
+ * @src: srcour where to copy from
+ * @n: nth
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *des = dest;
+	unsigned int x = 0;
 
-char* _memcpy(char *dest, const char *src, unsigned int n) {
-    char *start = dest;
-
-    while (n--) {
-        *dest++ = *src++;
-    }
-    
-    return start;
+	while (x < n)
+	{
+		*des = src[x];
+		des++;
+		x++;
+	}
+	return (des);
 }
