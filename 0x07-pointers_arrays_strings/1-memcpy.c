53866a4cdc365448e89126fff77d2451d96c7acd
danimohli <danimohlife@gmail.com>
2024-01-08 04:06:31 -0800

    memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,9 +7,7 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int x;
-
-	for (x = 0; x < n; x++)
-		*dest++ = src[x];
+	while (n--)
+		*dest++ = *src++;
 	return (dest);
 }
