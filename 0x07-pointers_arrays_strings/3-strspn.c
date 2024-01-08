c35a3324791c908dcda9affa9b62deac9a7d0ea0
danimohli <danimohlife@gmail.com>
2024-01-08 06:27:59 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+/**
+ * _strchr - locate a character in a string
+ * @s: string to search
+ * @c: character to search
+ * Return: pointer from the first found c
+ */
+char *_strchr(char *s, char c)
+{
+	int x;
+
+	for (x = 0; s[x] >= '\0'; x++)
+	{
+		if (s[x] == c)
+			return (s + x);
+	}
+	return ('\0');
+}
+/**
+ * _strspn - how many samme letter found in each other
+ * @s: string to search
+ * @accept: used to compare string
+ * Return: number of cha found
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count  = 0;
+
+	while (*s && strchr(accept, *s))
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
