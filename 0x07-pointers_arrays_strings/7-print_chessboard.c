844ffdb407bbf5634094058d4b15f467b893ce25
danimohli <danimohlife@gmail.com>
2024-01-08 19:47:06 -0800

    board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * print_chessboard - print chess board
  * @a: pointer to board
@@ -11,7 +11,7 @@ void print_chessboard(char (*a)[8])
 	for (x = 0; x < 8; x++)
 	{
 		for (y = 0; y < 8; y++)
-			putchar(a[x][y]);
-		putchar('\n');
+			_putchar(a[x][y]);
+		_putchar('\n');
 	}
 }
