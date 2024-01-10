7d8b5ad54308c93e17915230e2fc71ccc82c09c3
danimohli <danimohlife@gmail.com>
2024-01-10 12:55:22 -0800

    strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+/**
+ * strlen_recursion - strlen function
+ * @s: string
+ * Return: len of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
