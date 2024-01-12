d429ea317246fe34bc70e3e6fb91ed69ecfa7aed
danimohli <danimohlife@gmail.com>
2024-01-12 08:39:41 -0800

    number of argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the starting function
+ * @argc: argument count
+ * @argv: argument vector - one dimensional array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+
+	return (0);
+}
