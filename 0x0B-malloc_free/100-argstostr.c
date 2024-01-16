59f392a17dcc0c49a0128e5a4bcdaa626974417d
danimohli <danimohlife@gmail.com>
2024-01-16 11:48:57 -0800

    args

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+/**
+ * argstostr - argstostr string
+ * @ac: same as argc(argument count)
+ * @av: same as argv(argument vector)
+ * Return: av
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, totalLenght, currentPosition;
+	char *result;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	totalLength = 0;
+	for (i = 0; i < ac; ++i)
+		totalLength += strlen(av[i]) + 1;
+
+	result = (char *)malloc(totalLength * sizeof(char));
+
+	if (result == NULL)
+		return (NULL);
+
+	currentPosition = 0;
+	for (i = 0; i < ac; ++i)
+	{
+		strcpy(result + currentPosition, av[i]);
+		currentPosition += strlen(av[i]);
+		result[currentPosition++] = '\n';
+	}
+	result[currentPosition - 1] = '\0';
+	return (result);
+}
