8097d52f68bbd73422d0848fa3f724facb9e00c2
danimohli <danimohlife@gmail.com>
2024-01-17 08:26:04 -0800

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+/**
+ * _atoi - convert string num char number
+ * @s: point to string
+ * Return: convert number
+ */
+int _atoi(const char *str)
+{
+	int result = 0;
+	int sign = 1;
+
+	while (isspace(*str))
+		str++;
+
+	if (*str == '-' || *str == '+')
+		sign = (*str++ == '-') ? -1 : 1;
+
+	while (isdigit(*str))
+		result = result * 10 + (*str++ - '0');
+	return (sign * result);
+}
