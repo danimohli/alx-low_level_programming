806372a6f53378eddf302d0c8b0086601f798064
danimohli <danimohlife@gmail.com>
2024-01-18 11:18:47 -0800

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _calloc - calloc
+ * @nmemb: object
+ * @size: size
+ * Return: size * nmemb
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	memset(ptr, 0, nmemb * size);
+	return (ptr);
+}
