c6332df89aad2877931aec6223aff90a673c3555
danimohli <danimohlife@gmail.com>
2024-01-22 13:03:40 -0800

    new_d

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - create new dog
+ * @name: name of the name dog
+ * @age: age of it
+ * @owner: onwer of the dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_d;
+
+	new_d =(dog_t *)malloc(sizeof(dog_t));
+
+	if (new_d == NULL)
+		return (NULL);
+
+	new_d->name = strdup(name);
+	new_d->age = age;
+	new_d->owner = strdup(owner);
+
+	return (new_d);
+}
