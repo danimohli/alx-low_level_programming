5d234e7d63fcc7e0aa91f0ba352046c2c1360cee
danimohli <danimohlife@gmail.com>
2024-01-24 13:15:23 -0800

    pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	if (array && cmp != NULL)
+	if (array != NULL && cmp != NULL)
 	for (x = 0; x < size; x++)
 	{
 		if (cmp(array[x] != 0))
