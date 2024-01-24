61aa3b14288a50d917d0d6c0efc04613dd749d71
danimohli <danimohlife@gmail.com>
2024-01-24 12:33:34 -0800

    iterato

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * array_iterator - loop function
+ * @array: pointer to function
+ * @action: function pointer
+ * @size: size of array
+ * Return: void function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int x;
+
+	for (x = 0; x < size; x++)
+		action(array[x]);
+}
