a6a99f4cd911d2cdbae46f5a11e430d41a2c8c73
danimohli <danimohlife@gmail.com>
2024-01-25 15:54:52 -0800

    all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * print_all - print all values wiht 1 function
+ * @format: string character
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int x = 0, num;
+	char c, *str;
+	float floa;
+
+	va_start(args, format);
+	while (format[x] != '\0')
+	{
+		if (x == 9)
+			break;
+		if (format[x] == 'c')
+		{
+			c = (char) va_arg(args, int);
+			printf("%c\n", c);
+		}
+		else if (format[x] == 'i')
+		{
+			num = va_arg(args, int);
+			printf("%d\n", num);
+		}
+		else if (format[x] == 'f')
+		{
+			floa = (float) va_arg(args, double);
+			printf("%f\n", floa);
+		}
+		else if (format[x] == 's')
+		{
+			str = va_arg(args, char *);
+			if (str == NULL)
+				printf("(nil)\n");
+			else
+				printf("%s\n", str);
+		}
+		x++;
+	}
+	va_end(args);
+}
