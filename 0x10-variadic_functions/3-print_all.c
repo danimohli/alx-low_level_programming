e6ea09c3a4a7b61108066d3fca93e0859887262f
danimohli <danimohlife@gmail.com>
2024-01-25 16:24:46 -0800

    redited

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,6 +5,9 @@
  * print_all - print all values wiht 1 function
  * @format: string character
  */
+#include <stdarg.h>
+#include <stdio.h>
+
 void print_all(const char * const format, ...)
 {
 	va_list args;
@@ -20,25 +23,29 @@ void print_all(const char * const format, ...)
 			c = (char) va_arg(args, int);
 			printf("%c\n", c);
 		}
-		else if (format[x] == 'i')
+		else if (format[x] == 'i' || format[x] == 'f' || format[x] == 's')
 		{
-			num = va_arg(args, int);
-			printf("%d\n", num);
-		}
-		else if (format[x] == 'f')
-		{
-			floa = (float) va_arg(args, double);
-			printf("%f\n", floa);
-		}
-		else if (format[x] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				printf("(nil)\n");
-			else
-				printf("%s\n", str);
+			if (format[x] == 'i')
+			{
+				num = va_arg(args, int);
+				printf("%d\n", num);
+			}
+			else if (format[x] == 'f')
+			{
+				floa = (float) va_arg(args, double);
+				printf("%f\n", floa);
+			}
+			else if (format[x] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					printf("(nil)\n");
+				else if (str != NULL)
+					printf("%s\n", str);
+			}
 		}
 		x++;
 	}
 	va_end(args);
+	printf("\n");
 }
