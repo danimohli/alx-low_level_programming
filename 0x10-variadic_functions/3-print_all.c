ca02e38e502d08990aa18660f7fdc1ed1165c2aa
danimohli <danimohlife@gmail.com>
2024-01-26 07:21:44 -0800

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 /**
  * print_all - print all values wiht 1 function
- * @format: string character
+ * @format: string char
  */
 void print_all(const char * const format, ...)
 {
+	Arg current_arg;
 	va_list args;
-	int x = 0, num;
-	char c, *str;
-	float floa;
+	int x = 0;
 
 	va_start(args, format);
 	while (format[x] != '\0' && x < 9)
 	{
-		if (format[x] == 'c')
+		current_arg.type = format[x];
+		switch (current_arg.type)
 		{
-			c = (char) va_arg(args, int);
-			printf("%c\n", c);
-		}
-		else if (format[x] == 'i' || format[x] == 'f' || format[x] == 's')
-		{
-			if (format[x] == 'i')
-			{
-				num = va_arg(args, int);
-				printf("%d\n", num);
-			}
-			else if (format[x] == 'f')
-			{
-				floa = (float) va_arg(args, double);
-				printf("%f\n", floa);
-			}
-			else if (format[x] == 's')
-			{
-				str = va_arg(args, char *);
-				if (str == NULL)
+			case 'c':
+				current_arg.value.char_val = (char)va_arg(args, int);
+				printf("%c\n", current_arg.value.char_val);
+				break;
+			case 'i':
+				current_arg.value.int_val = va_arg(args, int);
+				printf("%d\n", current_arg.value.int_val);
+				break;
+			case 'f':
+				current_arg.value.float_val = (float)va_arg(args, double);
+				printf("%f\n", current_arg.value.float_val);
+				break;
+			case 's':
+				current_arg.value.str_val = va_arg(args, char *);
+				if (current_arg.value.str_val == NULL)
+				{
 					printf("(nil)\n");
-				else if (str != NULL)
-					printf("%s\n", str);
-			}
+				}
+				else if (current_arg.value.str_val != NULL)
+				{
+					printf("%s\n", current_arg.value.str_val);
+				}
+				break;
 		}
 		x++;
 	}
