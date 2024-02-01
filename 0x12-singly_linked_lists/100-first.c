e3527e2666048b04ffd889d47b14da43c3f35dd4
danimohli <danimohlife@gmail.com>
2024-02-01 07:10:10 -0800

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,5 +6,6 @@ void myC(void) __attribute__((constructor));
  */
 void myC(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
