f1de53e46e71042dcfbdb13fae47b47a4c829eb7
danimohli <danimohlife@gmail.com>
2024-02-05 17:03:31 -0800

    free listint nodes

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - free memory allocated to nodes of listint_t
+ * @head: point to head of list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);;
+		head = next;
+	}
+}
