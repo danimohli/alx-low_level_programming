04ae5922d9c3b5da2b59a1005e0414b45bc7be99
danimohli <danimohlife@gmail.com>
2024-02-05 17:36:46 -0800

    free with head to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint2 - free function
+ * @head: pointer to head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *hd, *d = *head;
+
+	while (d != NULL)
+	{
+		hd = d->next;
+		free(d);
+		d = hd;
+	}
+}
