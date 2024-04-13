9a7c8dc332c292c231e8949474c7b3c9f1e9af8e
danimohli <danimohlife@gmail.com>
2024-04-13 21:31:10 +0100

    key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+/**
+ * key_index - index function for hash
+ * @key: name of value
+ * @size: size
+ * Return: hash value
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value = hash_djb2(key);
+	unsigned long int ind = hash_value % size;
+
+	return (ind);
+}
