aebc2bbbef9e89cb85f769620c20e02a9ff9de1b
danimohli <danimohlife@gmail.com>
2024-04-14 04:53:29 +0100

    free_tab

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int x;
+	hash_node_t *node, *tp;
+
+	if (ht == NULL)
+		return;
+
+	for (x = 0; x < ht->size; x++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			tp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
