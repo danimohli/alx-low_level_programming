654582eff6e865b2f6503ed075602f59bdd4246e
danimohli <danimohlife@gmail.com>
2024-04-19 14:33:23 +0100

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,17 +13,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
 	if (ptr == NULL)
 	{
-		/*alloc = malloc(new_size);
+		alloc = malloc(new_size);
 		if (alloc == NULL)
-			return (NULL);*/
-		return (NULL);
+			return (NULL);
+		return (alloc);
 	}
 	alloc = malloc(new_size);
 	memcpy(alloc, ptr, old_size);
