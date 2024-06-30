df7ec89f02bb2c236158422f48667261e962508d
danimohli <danimohlife@gmail.com>
2024-06-30 20:49:54 +0100

    rewrite

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,68 @@
 #include <stdio.h>
+#include <string.h>
+
 /**
- * print_fibonacci - print fibonnanci
- * Return: void function
+ * add_strings - Adds two large numbers represented as strings.
+ * @num1: The first number as a string.
+ * @num2: The second number as a string.
+ * @result: The result string to store the sum.
+ *
+ * Return: A pointer to the result string.
  */
-void print_fibonacci(void)
+char *add_strings(char *num1, char *num2, char *result)
 {
-	unsigned long int a = 1;
-	unsigned long int b = 2;
-	unsigned long int temp;
+	int len1 = strlen(num1);
+	int len2 = strlen(num2);
+	int carry = 0, i, j, k = 0;
+	int max_len = len1 > len2 ? len1 : len2;
+
+	result[max_len + 1] = '\0';
+
+	for (i = len1 - 1, j = len2 - 1, k = max_len; k >= 0; i--, j--, k--)
+	{
+		int digit1 = i >= 0 ? num1[i] - '0' : 0;
+		int digit2 = j >= 0 ? num2[j] - '0' : 0;
+		int sum = digit1 + digit2 + carry;
+
+		result[k] = (sum % 10) + '0';
+		carry = sum / 10;
+	}
+
+	if (result[0] == '0')
+		memmove(result, result + 1, max_len);
+	return (result);
+}
+
+/**
+ * print_fibonacci - Prints the first n Fibonacci numbers.
+ * @n: The number of Fibonacci numbers to print.
+ */
+void print_fibonacci(int n)
+{
+	char fib1[200] = "1";
+	char fib2[200] = "2";
+	char result[200];
 	int i;
 
-	printf("1, 2");
+	printf("%s, %s", fib1, fib2);
 
-	for (i = 3; i <= 98; i++)
+	for (i = 3; i <= n; i++)
 	{
-		temp = a + b;
-		printf(", %lu", temp);
-
-		a = b;
-		b = temp;
+		add_strings(fib1, fib2, result);
+		printf(", %s", result);
+		strcpy(fib1, fib2);
+		strcpy(fib2, result);
 	}
 	printf("\n");
 }
+
 /**
- * main - main function
- * Return: 0
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
-	print_fibonacci();
+	print_fibonacci(98);
 	return (0);
 }
