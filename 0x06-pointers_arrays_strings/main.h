3814609849d1bfdfe9ee6899ed06a1b9983023a8
danimohli <danimohlife@gmail.com>
2024-07-07 13:54:50 +0100

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 void print_number(int);
 char *_strcat(char *dest, char *src);
@@ -12,5 +13,6 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
