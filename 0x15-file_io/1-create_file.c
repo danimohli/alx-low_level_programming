88f16b3665d2e0753f96240d8edb933d71320901
danimohli <danimohlife@gmail.com>
2024-08-12 12:47:14 +0100

    re-writ to create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,30 +7,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t writebyte, size = strlen(text_content);
 	int fd;
+	size_t len;
+	ssize_t w;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
-		writebyte = write(fd, text_content, size);
-		if (writebyte == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(fd);
+		return (1);
+	}
 
-		if (writebyte != size)
-		{
-			close(fd);
-			return (-1);
-		}
+	len = strlen(text_content);
+	w = write(fd, text_content, len);
+	if (w == -1 || (size_t) w != len)
+	{
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 	return (1);
